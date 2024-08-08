e8aa6f685f7758df039201ace1740ec2ddcc2b40
Gabriel Nützi <gnuetzi@gmail.com>
2024-08-08 17:00:26 +0200

    chore: simple fixes for gh-pages publish

--- tools/prepare-gh-pages.sh
+++ tools/prepare-gh-pages.sh
@@ -5,9 +5,18 @@ set -u
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
+name="${1:-demo}"
+presentation="${2:-presentation-1}"
+pages_dir="docs/gh-pages"
+target="$pages_dir/$name"
+
 cd "$ROOT_DIR"
 just init
-just sync
+just sync "$presentation"
+
 rm -rf build/node_modules
-rm -rf docs/
-cp build docs
+rm -rf "$target" || true
+mkdir -p ""
+cp build "$target"
+
+echo "Create a PR to branch 'publish' to merge only changes in '$pages_dir'."
